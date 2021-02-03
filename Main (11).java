12df28ff44f5888f13a1134b4cdbe386a9612fb7
31devi <77260949+31devi@users.noreply.github.com>
2021-02-03 14:16:40 +0530

    Add files via upload

---
+++ Main (11).java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main{
+  public static void main(String[]args){
+    Scanner kbd= new Scanner(System.in);
+    int n=kbd.nextInt(); 
+      int y=kbd.nextInt();
+          System.out.print( n * (y - 1) + 1);
+}
+}
